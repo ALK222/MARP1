c80da993c3cd9baaab8c2d117cad286a1e494333
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2024-01-09 19:41:35 +0100

    27.-Repartiendo_Paquetes

---
+++ juez/grafos/27.-Repartiendo_Paquetes/Main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <fstream>
+#include "DigrafoValorado.h"
+#include "IndexPQ.h"
+
+#define MAX_TIEMPO 1000000000
+
+class TecladosRotos
+{
+private:
+    DigrafoValorado<int> const grafo;
+    int const ini;
+    IndexPQ<int> pq;
+    std::vector<long long int> dist;
+    int min;
+
+    void relaja(AristaDirigida<int> a)
+    {
+        int v = a.desde();
+        int w = a.hasta();
+
+        if (dist[w] > dist[v] + a.valor())
+        {
+            dist[w] = dist[v] + a.valor();
+            pq.update(w, dist[w]);
+        }
+    }
+    void contarEnlaces(DigrafoValorado<int> g)
+    {
+        dist[ini] = 0;
+        pq.push(ini, 0);
+
+        while (!pq.empty())
+        {
+            int v = pq.top().elem;
+            pq.pop();
+            for (AristaDirigida<int> a : g.ady(v))
+            {
+                relaja(a);
+            }
+        }
+    }
+
+public:
+    TecladosRotos(DigrafoValorado<int> const &g, int i) : grafo(g), ini(i), pq(g.V()), dist(g.V(), MAX_TIEMPO), min(0)
+    {
+    }
+
+    void resolver(std::vector<int> dest)
+    {
+        contarEnlaces(grafo.inverso());
+        std::vector<int> vuelta;
+
+        for (int i = 0; i < dist.size(); ++i)
+        {
+            vuelta.push_back(dist[i]);
+            dist[i] = MAX_TIEMPO;
+        }
+
+        contarEnlaces(grafo);
+
+        for (int i : dest)
+        {
+            if (dist[i] == MAX_TIEMPO || vuelta[i] == MAX_TIEMPO)
+            {
+                min = MAX_TIEMPO;
+                return;
+            }
+            min += dist[i] + vuelta[i];
+        }
+    }
+
+    bool posible()
+    {
+        return min != MAX_TIEMPO;
+    }
+
+    long long int tiempo()
+    {
+        return min;
+    }
+};
+
+bool resuelveCaso()
+{
+    int N;
+    std::cin >> N;
+
+    if (!std::cin)
+        return false;
+
+    int E;
+    std::cin >> E;
+    DigrafoValorado<int> grafo(N);
+    for (int ar = 0; ar < E; ++ar)
+    {
+        int v, w, c;
+        std::cin >> v >> w >> c;
+        grafo.ponArista({v - 1, w - 1, c});
+    }
+
+    int ini;
+    std::cin >> ini;
+    ini--;
+
+    int paquetes;
+    std::cin >> paquetes;
+    std::vector<int> destinatarios(paquetes);
+    for (int i = 0; i < paquetes; i++)
+    {
+        int destinatario;
+        std::cin >> destinatario;
+        destinatarios[i] = destinatario - 1;
+    }
+
+    TecladosRotos problema(grafo, ini);
+    problema.resolver(destinatarios);
+    if (problema.posible())
+    {
+        std::cout << problema.tiempo() << "\n";
+    }
+    else
+    {
+        std::cout << "Imposible\n";
+    }
+
+    return true;
+}
+int main()
+{
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso())
+        ;
+
+        // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
